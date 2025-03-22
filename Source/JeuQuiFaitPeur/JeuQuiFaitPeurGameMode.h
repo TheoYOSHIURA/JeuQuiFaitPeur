47a844d0d4aeb93cffd42775cddcd8b98602b8c8
Theo_Yoshiura <theo.yr@gmail.com>
2025-03-22 15:34:49 +0100

    Post-nuke project

---
+++ Source/JeuQuiFaitPeur/JeuQuiFaitPeurGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JeuQuiFaitPeurGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AJeuQuiFaitPeurGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AJeuQuiFaitPeurGameMode();
+};
+
+
+
